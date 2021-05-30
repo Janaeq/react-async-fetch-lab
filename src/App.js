19ad8776ddf69dde738b9eebd6c909bc2e74eb16
Janae Quinones <janaemassa@gmail.com>
2021-05-30 18:30:46 -0400

    commit tree b93f67a7c7d8bcd374c336a09a9e405c30d7534c

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    componentDidMount() {
+        let url = 'http://api.open-notify.org/astros.json'
+        fetch(url)
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                number: data.number,
+                people: data.people
+            })
+        })
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <h1>Hello</h1>
+        )
+    }
+}
